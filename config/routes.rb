74f5f16b4c952a991951ad1aebdfc696ec48404b
Heinz Aufner <heinzaufner@hotmail.com>
2017-01-20 23:24:12 +1100

    Chapter 9. Added user submitted and voted by for functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   resource :session
   resources :stories do
     collection do
@@ -6,7 +8,9 @@ Rails.application.routes.draw do
     end
     resources :votes
   end
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root "stories#index"
 end
